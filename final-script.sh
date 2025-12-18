025f69167ab001a9dcb20d9d0988d3a8f7fd4ba8
Artem Ustiantsev <119280553+austiantsev@users.noreply.github.com>
2025-12-18 16:54:10 +0400

    Corrections-1

--- final-script.sh
+++ final-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Привет"
-echo "Мое имя" $USER
-echo "Сейчас я нахожусь в" $PWD
-echo "Мой домашний каталог" $HOME
+echo "Мое имя:" $USER
+echo "Сейчас я нахожусь в:" $PWD
+echo "Мой домашний каталог:" $HOME
 echo "Моя ОС" 
 cat /etc/os-release
+echo "Мое время работы"
 uptime
